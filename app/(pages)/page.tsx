3df522f5834966aaf333e2b2afa7d979230a281a
szbr1 <szb8480@gmail.com>
2025-11-11 00:28:10 +0500

    refactor: include type safety for better auth session object

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,8 +1,36 @@
+"use client"
+import { authClient } from '@/lib/auth-client'
+import { SessionProps } from '@/types/types'
+import React, { useEffect, useState } from 'react'
 
-export default function Home() {
+function Page() {
+
+  const [session , setSession] = useState<SessionProps | null>(null)
+  useEffect(()=>{
+     (async()=>{
+     const data = await authClient.getSession();
+     if(data && data.data && data.data.user){
+       setSession(data.data.user)
+    }
+      
+  })()
+  }, [])
+
+  if (!session) return <div>Loading...</div> 
+
+  console.log(session)
   return (
-   <div>
-     Landing Page
-   </div>
-  );
+    <div className='p-3 h-screen w-full'>
+      
+     <b>Name:</b>  {session.name}
+      <br />
+     <b>Email:</b> {session.email}
+      <br />
+     <b>Image:</b> {session.image}
+      <br />
+     <b>Email verified:</b> {session.emailVerified}
+    </div>
+  )
 }
+
+export default Page
