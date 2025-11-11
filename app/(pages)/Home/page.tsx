eaee9fa2e5c2bbc54df8564f5966df4ab81bb330
szbr1 <szb8480@gmail.com>
2025-11-11 13:59:02 +0500

    feat: homepage styling applied

--- app/(pages)/Home/page.tsx
+++ app/(pages)/Home/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import Header from '../_components/Header'
+import { Button } from '@/components/ui/button'
+import HomeGrid from './_components/HomeGrid'
 
 function page() {
   return (
-    <div className='overflow-hidden'>
-       Home
+    <div className='overflow-hidden pr-3'>
+      <div className='flex justify-end items-center gap-4'>
+         <Button className='cursor-pointer' variant={"outline"} >Create Folder</Button>
+         <Button className='bg-default hover:bg-default' >Create File</Button>
+      </div>
+
+      <p className='text-xs py-3 text-gray-300'>Layer of security</p>
+       <HomeGrid />
+   
+      <p className='text-xs py-3 text-gray-300'>Subscriptions of security</p>
+
+    
     </div>
   )
 }
